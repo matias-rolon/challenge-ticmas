373f86e962f590afe56767093c433804f5af570a
Matias Rolon <matiasrolon1301@gmail.com>
2024-05-01 17:41:41 -0300

    Create API

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,47 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { TasksService } from './tasks.service'
+import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private tasksService: TasksService) { }
+
+    @Get()
+    getAllTasks() {
+        return this.tasksService.getAllTasks()
+    }
+
+    @Get(":id")
+    getByID(@Param('id') id: string) {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Get("status/:status")
+    getByStatus(@Param('status') status: string) {
+        return this.tasksService.getTaskByStatus(status)
+    }
+
+    @Get(":id/days-passed")
+    getDaysPassed(@Param("id") id: string) {
+        return this.tasksService.getDaysPassedTask(id);
+    }
+
+
+    @Post()
+    createTask(@Body() newTask: CreateTaskDto) {
+        console.log(newTask);
+        return this.tasksService.createTasks(newTask.title, newTask.description)
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id: string) {
+        this.tasksService.deleteTasks(id);
+    }
+
+    @Patch(":id")
+    updateTask(@Param('id') id: string, @Body() updatedFields: UpdateTaskDto) {
+        return this.tasksService.updateTasks(id, updatedFields);
+    }
+
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from "./task.entity";
+import { v4 } from "uuid";
+import { UpdateTaskDto } from './dto/task.dto';
+
+@Injectable()
+export class TasksService {
+
+    private tasks: Task[] = [{
+        id: '1',
+        title: 'frist task', 
+        description: 'This is a description',
+        status: TaskStatus.PENDING,
+        creationDate: new Date(),
+    }]
+
+    getAllTasks() {
+        return this.tasks;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    getTaskByStatus(status: string) {
+        return this.tasks.filter(task => task.status === status);
+    }
+    getDaysPassedTask(id: string) {
+        const task = this.tasks.find(task => task.id === id);
+    
+        const creationDate = new Date(task.creationDate);
+        const currentDate = new Date();
+        
+        const creationDateMs = creationDate.getTime();
+        const currentDateMs = currentDate.getTime();
+        
+        const differenceMs = currentDateMs - creationDateMs;
+        
+        const daysPassed = Math.floor(differenceMs / (1000 * 60 * 60 * 24));
+        
+        return daysPassed;
+    }
+    
+
+    createTasks(title: string, description: string) {
+        const task = {
+            id: v4(),
+            title,
+            description,
+            status: TaskStatus.PENDING,
+            creationDate: new Date
+        }
+        this.tasks.push(task);
+
+        return task;
+    }
+
+    deleteTasks(id: string) {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+
+    updateTasks(id: string, updateFields: UpdateTaskDto): Task {
+        const task = this.getTaskById(id)
+        const newTask = Object.assign(task, updateFields);
+        this.tasks = this.tasks.map(task => task.id === id ? newTask : task)
+        return newTask;
+    }
+
+}
